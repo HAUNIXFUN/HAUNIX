1c9c483398aa939ceff986f3e54dbb2e90ebf85a
HOOTONE <eduardermakov769@gmail.com>
2025-11-07 20:04:12 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        MINECODE
+import os        HOOTONE
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
